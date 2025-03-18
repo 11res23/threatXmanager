9f867bd40a58ad23714be4712e927428bcb3dada
Luiz Correa <zcorrea357@gmail.com>
2025-03-18 17:01:35 +0000

    feat(init): repo

---
+++ exemplo/cpe_run.py
@@ -0,0 +1,6 @@
+from threatxmanager.modules.external.cpe_connector_module import CPEConnectorModule
+
+if __name__ == "__main__":
+    "init"
+    cpe_module = CPEConnectorModule()
+    cpe_module.run()
